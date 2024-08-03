d2da8ed4219371ece1e90762e3168ff69ee8dd64
chukwurie <chukwuriec@gmail.com>
2024-08-03 06:25:16 -0700

    TaskApp

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark text-white">
+      <div className="container">
+        <a className="navbar-brand d-flex align-content-center gap-2" href="/">
+          <h1 className="fs-4 text-uppercase text-white">Task Management</h1>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
